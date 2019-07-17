c8eaaac235d8a80fef457510e8732fdce71042ba
ShoheiImamura <s3imamura@gmail.com>
2019-07-18 01:08:45 +0900

    first commit

--- sampleapp/resources/views/home.blade.php
+++ sampleapp/resources/views/home.blade.php
@@ -4,5 +4,12 @@
     </head>
     <body>
         こんにちは！
+        @if (Auth::check())
+        {{\Auth::user()->name}}さん<br/>
+        <a href="/auth/logout">ログアウト</a>
+        @else
+        ゲストさん<br/>
+        <a href="/auth/register">会員登録</a>
+        @endif
     </body>
 </html>
